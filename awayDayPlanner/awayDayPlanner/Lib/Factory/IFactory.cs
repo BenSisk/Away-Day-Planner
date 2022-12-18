5e5cae9d3c1e2914a89cfd17e08f18dbb8fc70af
Anthony Donnelly <44259387+Amzo@users.noreply.github.com>
2022-12-18 07:35:54 +0000

    Add the template for factory
    Added the template for the factory and added skeletal code for Login

---
+++ awayDayPlanner/awayDayPlanner/Lib/Factory/IFactory.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace awayDayPlanner.Lib.Factory
+{
+    internal interface IIFactory
+    {
+        void getInstance();
+        void getCredentials();
+        void verifyCredentials();
+        void createUser();
+
+    }
+}
+
