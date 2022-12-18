86a7e129aa7aa9601b634b303093ba2552241327
Anthony Donnelly <44259387+Amzo@users.noreply.github.com>
2022-12-18 07:45:21 +0000

    Added in template for the Register Singleton Factory

---
+++ awayDayPlanner/awayDayPlanner/Lib/Factory/Register.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+// object factory used for creating user objects
+namespace awayDayPlanner.Lib.Factory
+{
+    internal class Register : IFactory
+    {
+        private static Register instance = null;
+        private static readonly object padlock = new object();
+
+        Register()
+        {
+
+        }
+
+        public static Register getInstance
+        {
+            get
+            {
+                lock (padlock)
+                {
+                    if (instance == null)
+                    {
+                        instance = new Register();
+                    }
+                    return instance;
+                }
+            }
+        }
+
+        public void getCredentials()
+        {
+
+        }
+
+        public void verifyCredentials()
+        {
+
+        }
+
+        public void createUser()
+        {
+
+        }
+
+        private void hashPassword()
+
+        {
+
+        }
+    }
+}
