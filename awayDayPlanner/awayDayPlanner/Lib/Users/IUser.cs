9a891e09ab2ac6824d7143cbbcd4ad8a6d89baf0
Anthony Donnelly <44259387+Amzo@users.noreply.github.com>
2022-12-18 08:02:02 +0000

    Template code for Users
    Added in the skeletal code for users

---
+++ awayDayPlanner/awayDayPlanner/Lib/Users/IUser.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace awayDayPlanner.Lib.Users
+{
+    internal interface IUser
+    {
+        int id
+        {
+            get; set;
+        }
+
+        string firstname
+        {
+            get; set;
+        }
+
+        string lastname
+        { 
+            get; set;
+        }
+        string email
+        {
+            get; set;
+        }
+
+        DateTime dob
+        {
+            get; set;
+        }
+
+        void verifyDetails();
+    }
+}
---
+++ awayDayPlanner/awayDayPlanner/Lib/Users/User.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace awayDayPlanner.Lib.Users
+{
+    internal class User : IUser
+    {
+        public int id { get; set; }
+        public string firstname { get; set; }
+        public string lastname { get; set; }
+        public string email { get; set; }
+        public DateTime dob { get; set; }
+
+
+        public void verifyDetails()
+        {
+
+        }
+    }
+}
