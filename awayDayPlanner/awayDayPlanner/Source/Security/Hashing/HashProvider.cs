dc8188e0f56b984551cb1d6391aa8545e64086a3
Anthony Donnelly <44259387+Amzo@users.noreply.github.com>
2022-12-22 08:24:56 +0000

    Place salting code into the Security folder

---
+++ awayDayPlanner/awayDayPlanner/Source/Security/Hashing/HashProvider.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace awayDayPlanner.Source.Security
+{
+    class HashProvider
+    {
+        private static string BytesToString(byte[] bytes)
+        {
+            StringBuilder builder = new StringBuilder();
+
+            for (int i = 0; i <bytes.Length; i++)
+            {
+                builder.Append(bytes[i].ToString("x2"));
+            }
+
+            return builder.ToString();
+        }
+        public static string Hash(string input, IHasher hasher)
+        {
+            return BytesToString(hasher.Hash(Encoding.Default.GetBytes(input)));
+        }
+    }
+}
